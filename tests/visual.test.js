191c5fc6822174d4105cb43fb5ec14b82c443e32
Nat-ka02 <nataliapalka19@gmail.com>
2025-11-24 20:15:50 +0100

    page-rendering-test

--- tests/visual.test.js
+++ tests/visual.test.js
@@ -6,17 +6,16 @@ test('Page Rendering', async ({ page }) => {
     await page.goto(process.env.HOME_PAGE_URL);
 
     await expect(page.getByRole('heading', { name: 'Lessons Countdown' })).toBeVisible();
-    await expect(page.locator('.menu-button-container')).toBeVisible();
+    await expect(page.locator('.menu-button')).toBeVisible();
 
-    await page.locator('.menu-button-container').click();
+    await page.locator('.menu-button').click();
 
-    await expect(page.locator('.open')).toBeVisible();
+    await expect(page.locator('#aside.open')).toBeVisible();
 
-    await expect(page.locator('div:has-text("Edit mode")')).toBeVisible();
-    await expect(page.locator('#edit-mode')).toBeVisible();
-    await expect(page.locator('div:has-text("Dark mode")')).toBeVisible();
-    await expect(page.locator('#dark-mode')).toBeVisible();
-    await expect(page.locator('div:has-text("Invert colors on start")')).toBeVisible();
-    await expect(page.locator('#invert-colors')).toBeVisible();
+
+    await expect(page.locator('div:has(#edit-mode):has-text("Edit mode")')).toBeVisible();
+    await expect(page.locator('div:has(#dark-mode):has-text("Dark mode")')).toBeVisible();
+    await expect(page.locator('div:has(#invert-colors):has-text("Invert colors on start")')).toBeVisible();
 
 });
+
